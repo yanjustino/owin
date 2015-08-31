227a82b5bd1fe276dc90ee7b9d509aa09336e994
yan de lima justino <yan_limaj@hotmail.com>
2015-08-31 05:32:00 -0300

    Hellow World from Owin

--- src/MidwareCodeSchool/Midware.cs
+++ src/MidwareCodeSchool/Midware.cs
@@ -4,6 +4,8 @@ using System.Threading.Tasks;
 
 namespace MidwareCodeSchool
 {
+    using System.IO;
+    using System.Text;
     using AppFunc = Func<IDictionary<string, object>, Task>;
 
     public class Midware
@@ -15,12 +17,32 @@ namespace MidwareCodeSchool
 
         public Midware(AppFunc next)
         {
-            _next = next;
+            _next = Middleware();
         }
 
         public async Task Invoke(IDictionary<string, object> environment)
         {
             await _next.Invoke(environment);
+        }
+
+        public AppFunc Middleware()
+        {
+            AppFunc result =
+                (IDictionary<string, object> e) =>
+                {
+
+                    var response = (Stream)e["owin.ResponseBody"];
+                    const string message = "Hello World from Owin";
+                    var b = Encoding.UTF8.GetBytes(message);
+
+                    var headers = (IDictionary<string, string[]>)e["owin.ResponseHeaders"];
+                    headers["Content-Length"] = new[] { b.Length.ToString() };
+                    headers["Content-Type"] = new[] { "text/html" };
+
+                    return response.WriteAsync(b, 0, b.Length);
+                };
+
+            return result;
         }
     }
 }
