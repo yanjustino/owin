6f9ba5f9d220d926ba86833a1142e48824ceff0a
yan de lima justino <yan_limaj@hotmail.com>
2015-08-31 04:56:09 -0300

    Estrutura básica de um Midware Owin

---
+++ src/MidwareCodeSchool/Midware.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MidwareCodeSchool
+{
+    using AppFunc = Func<IDictionary<string, object>, Task>;
+
+    public class Midware
+    {
+        private AppFunc _next;
+
+        public Midware(AppFunc next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(IDictionary<string, object> environment)
+        {
+            await _next.Invoke(environment);
+        }
+    }
+}
