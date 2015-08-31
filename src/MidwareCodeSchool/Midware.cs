33986d4fadc99b083d1d5ac28d1c20a1a0c0d27f
yan de lima justino <yan_limaj@hotmail.com>
2015-08-31 05:44:25 -0300

    hellow world

--- src/MidwareCodeSchool/Midware.cs
+++ src/MidwareCodeSchool/Midware.cs
@@ -4,23 +4,50 @@ using System.Threading.Tasks;
 
 namespace MidwareCodeSchool
 {
+    using System.IO;
+    using System.Text;
     using AppFunc = Func<IDictionary<string, object>, Task>;
 
     public class Midware
     {
-        ///The primary interface in OWIN is called the application delegate or AppFunc. 
-        /// An application delegate takes the IDictionary<string, object> environment and returns a Task 
-        /// when it has finished processing.
         private AppFunc _next;
 
         public Midware(AppFunc next)
         {
-            _next = next;
+            _next = Middleware();
         }
 
         public async Task Invoke(IDictionary<string, object> environment)
         {
             await _next.Invoke(environment);
+        }
+
+        /// <summary>
+        /// The Environment dictionary stores information about the request, the response, 
+        /// and any relevant server state.  The server is responsible for providing body 
+        /// streams and header collections for both the request and response in the initial call.  
+        /// The application then populates the appropriate fields with response data, 
+        /// writes the response body, and returns when done.
+        /// </summary>
+        /// <returns></returns>
+        public AppFunc Middleware()
+        {
+            AppFunc result =
+                (IDictionary<string, object> e) =>
+                {
+
+                    var response = (Stream)e["owin.ResponseBody"];
+                    const string message = "Hello World from Owin";
+                    var b = Encoding.UTF8.GetBytes(message);
+
+                    var headers = (IDictionary<string, string[]>)e["owin.ResponseHeaders"];
+                    headers["Content-Length"] = new[] { b.Length.ToString() };
+                    headers["Content-Type"] = new[] { "text/html" };
+
+                    return response.WriteAsync(b, 0, b.Length);
+                };
+
+            return result;
         }
     }
 }
